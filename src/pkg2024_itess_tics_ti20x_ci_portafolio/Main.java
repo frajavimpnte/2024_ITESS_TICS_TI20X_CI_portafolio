4ab9a07b55f183fd4d02be7e5bbe0ba517bdfa26
frajavimopu <frajavimopu@gmail.com>
2024-05-24 14:32:20 -0500

    Inicio del proyecto.

---
+++ src/pkg2024_itess_tics_ti20x_ci_portafolio/Main.java
@@ -0,0 +1,17 @@
+/*
+ * ITESS-TICS
+ * Enero-Junio 2024
+ * Calculo Integral portafolio
+ * TI-20X
+ * 24/05/2024
+ * by FJMP
+ *
+ */
+package pkg2024_itess_tics_ti20x_ci_portafolio;
+
+public class Main {
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hello FX");
+    }
+}
